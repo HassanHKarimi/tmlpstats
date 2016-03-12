3a7dddac522c4c7026f44b5c578eda2ca2f3eb53
Peter <peter.argue@gmail.com>
2016-03-11 18:20:40 -0800

    another one

--- src/resources/views/partials/alerts.blade.php
+++ src/resources/views/partials/alerts.blade.php
@@ -1,33 +1,36 @@
-<div class="message-center">
-    @foreach ($alerts as $alert)
-        <?php
-        $prefix = '';
-        switch ($alert->type) {
-            case 'error':
-                $class = 'danger';
-                break;
-            case 'warning':
-                $class = 'warning';
-                break;
-            case 'success':
-                $class = 'success';
-                break;
-            case 'info':
-            default:
-                $class = 'info';
-                break;
-        }
-        ?>
-        <div class="alert alert-{{ $class }} alert-message" role="alert">
-            @if ($alert->isDismissable())
-                <a href="#" class="close" data-dismiss="alert">&times;</a>
-            @endif
+<div class="panel panel-default message-center">
+    <div class="panel-heading">Messages</div>
+    <div class="panel-body">
+        @foreach ($alerts as $alert)
+            <?php
+            $prefix = '';
+            switch ($alert->type) {
+                case 'error':
+                    $class = 'danger';
+                    break;
+                case 'warning':
+                    $class = 'warning';
+                    break;
+                case 'success':
+                    $class = 'success';
+                    break;
+                case 'info':
+                default:
+                    $class = 'info';
+                    break;
+            }
+            ?>
+            <div class="alert alert-{{ $class }} alert-message" role="alert">
+                @if ($alert->isDismissable())
+                    <a href="#" class="close" data-dismiss="alert">&times;</a>
+                @endif
 
-            @if ($prefix)
-                <span class="message-prefix">{{ $prefix }}</span>
-            @endif
+                @if ($prefix)
+                    <span class="message-prefix">{{ $prefix }}</span>
+                @endif
 
-            <span class="message">{{ $alert->message }}</span>
-        </div>
-    @endforeach
+                <span class="message">{{ $alert->message }}</span>
+            </div>
+        @endforeach
+    </div>
 </div>
